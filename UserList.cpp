0f5d3e8e3b3995398a8e7ed0ad37ee81da76c9cf
eugene <eugene.rublenko@gmail.com>
2017-04-03 20:10:36 +0300

    add read_json and print_json methods to UserList class

--- UserList.cpp
+++ UserList.cpp
@@ -1,4 +1,15 @@
 #include "UserList.hpp"
 
 UserList::UserList(): userlist(json({})), filename("userlist.txt") {};
+
 UserList::~UserList() {};
+
+void UserList::read_json() {
+	ifstream input(filename);
+	input >> userlist;
+	if(input.is_open()) input.close();
+}
+
+void UserList::print_json() {
+	cout << userlist << endl;
+}
--- UserList.hpp
+++ UserList.hpp
@@ -11,10 +11,14 @@ class UserList {
 private:
 	json userlist;
 	string filename;
-	ifstream userlist_in;
-	ofstream userlist_out;
+	//TO-DO: it looks like we don't need members above
+	//ifstream userlist_in;
+	//ofstream userlist_out;
 
 public:
 	UserList();
 	~UserList();
+	void read_json();
+	//method for debug purposes
+	void print_json();
 };
