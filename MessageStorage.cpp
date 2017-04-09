915f9929e6380680b5c38c5f1fdcc01dbc53a4af
eugene <eugene.rublenko@gmail.com>
2017-04-09 14:22:26 +0300

    implement is_thread_connected and connect_thread methods in MessageStorage class

--- MessageStorage.cpp
+++ MessageStorage.cpp
@@ -1,1 +1,15 @@
 #include "MessageStorage.hpp"
+
+bool MessageStorage::is_thread_connected(int id_to_check) {
+	for (int i = 0; i < threads.size(); i++) 
+		if(threads[i].get_thread_id() == id_to_check) return true;
+	return false;
+}
+
+void MessageStorage::connect_thread(int id) {
+	if(!is_thread_connected(id)) {
+		MessageThread t(id,3);
+		t.read_whole_json();
+		threads.push_back(t);
+	}
+}
--- MessageStorage.hpp
+++ MessageStorage.hpp
@@ -1,5 +1,10 @@
 #include "MessageThread.hpp"
 
 class MessageStorage {
-
+private:
+	vector<MessageThread> threads;
+public:
+	bool is_thread_connected(int id_to_check);
+	//change read method when you will use buffer zone
+	void connect_thread(int id);
 };
