96b192dfd564fddf52f21779d849ee5232c6ea74
eugene <eugene.rublenko@gmail.com>
2017-04-09 15:06:53 +0300

    add some notes in MessageThread class

--- MessageStorage.hpp
+++ MessageStorage.hpp
@@ -3,10 +3,19 @@
 class MessageStorage {
 private:
 	vector<MessageThread> threads;
+	
 public:
+	//private?
 	bool is_thread_connected(int id_to_check);
 	//change read method when you will use buffer zone
 	void connect_thread(int id);
-
+	
+	//private?
 	void push_message_to_thread(int thread_id, string msg_json);
+	
+	void recieve_message(int thread_id, string username, string ip, string body);
+
+	//string get_whole_buffer
+
+	//string get_buffer_since
 };
