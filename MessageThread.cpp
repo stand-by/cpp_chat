52f1264e8b6432ba10aadb736bf2d558627206f2
eugene <eugene.rublenko@gmail.com>
2017-04-09 10:59:41 +0300

    add constructor and necessary properties to MessageThread class

--- MessageThread.cpp
+++ MessageThread.cpp
@@ -1,1 +1,6 @@
 #include "MessageThread.hpp"
+
+MessageThread::MessageThread(int id, int amount)
+:thread_id(id), amount_messages_to_hold(amount), amount_messages_shifted(0) {
+	filename = std::to_string(id)+".json";
+}
--- MessageThread.hpp
+++ MessageThread.hpp
@@ -1,5 +1,10 @@
 #include "JsonWrapper.hpp"
 
 class MessageThread: public JsonWrapper {
-
+private:
+	int thread_id;
+	int amount_messages_to_hold;
+	int amount_messages_shifted;
+public:
+	MessageThread(int id, int amount);
 };
